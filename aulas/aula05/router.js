4ddf95f0f3aeb78532c16ffca3e09867af394101
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-03-15 10:50:29 -0300

    finalizando aula 05

--- aulas/aula05/router.js
+++ aulas/aula05/router.js
@@ -2,8 +2,45 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = router;
 
 router.get("/produtos", function(req, res){
-    res.json([])
+    res.json([]); // 200 e JSON
 })
+
+router.get("/produtos/:produtoId", function(req, res){
+    if(req.params.produtoId == 1){
+        res.json({});
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"});
+    }
+})
+
+router.post("/produtos", function(req, res){
+    if(req.body && req.body.nome && req.body.preco) {
+        res.status(201).json({});
+    } else {
+        res.status(422).json({msg: "Nome e/ou preco do produto necessários"});
+    }
+}) 
+
+router.put("/produtos", function(req, res) {
+    res .json({});
+})
+
+router.put("/produtos/:produtoId", function(req, res) {
+    if(req.params.produtoId == 1) {
+        res.json({});
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"})
+    }
+})
+
+router.delete("/produtos/:produtoId", function(req, res){
+    if(req.params.produtoId == 1) {
+        res.status(204).end();
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"})
+    }
+})
+
+module.exports = router;
