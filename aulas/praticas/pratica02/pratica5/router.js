432089eaf94822c5df93350452fd6dcc30210636
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-03-21 23:04:29 -0300

    finalizando dever pratica 5

--- aulas/praticas/pratica02/pratica5/router.js
+++ aulas/praticas/pratica02/pratica5/router.js
@@ -8,8 +8,24 @@ router.get("/produtos", function(req, res) {
     res.json(produtos); 
   });
 
+
+  router.post('/produtos', function(req, res){
+    if(req.body && req.body.nome && req.body.preco) {
+        const novo = {
+            id: produtos.length + 1,
+            nome: req.body.nome,
+            preco: req.body.preco
+        }
+        produtos.push(novo);
+        res.status(201).json(novo);
+    } else {
+        res.status(422).json({msg: "Produto não encontrado"});
+        
+    }
+});
+
 router.get("/produtos/:produtoid", function(req, res){
-    const encontrado = produtos.find(item => item.id == req.params.produtoid)
+    const encontrado = produtos.find(item => item.id == req.params.produtoid);
     if(encontrado) {
         res.json(encontrado);
     } else {
@@ -17,18 +33,27 @@ router.get("/produtos/:produtoid", function(req, res){
     }
 });
 
-router.put('/produtos', function(req, res){
-    const encontrado = produtos.find(item => item.id == req.params.produtoid)
+router.put('/produtos/:produtoid', function(req, res){
+    const encontrado = produtos.find(item => item.id == req.params.produtoid);
     if(encontrado) {
         encontrado.nome = req.body.nome;
         encontrado.preco = req.body.preco;
         res.json({encontrado});
- } else {
-    res.status(404).json({msg: "Produto não encontrado"})
- }
-})
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"});
+    }
+});
 
 
+router.delete("/produtos/:produtoid", function (req, res) {
+    const posicao = produtos.findIndex(item => item.id == req.params.produtoid);
+    if(posicao >= 0){
+        produtos.splice(posicao, 1);
+        res.status(204).end();
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"});
+    }
+});
 
 
 
